d0d5ffa9c068580bf200cf1f1146c190bee33b4d
andystrn <an.strnadlova@gmail.com>
2023-02-24 14:38:25 +0100

    creating Palette component and using it to rendering all palettes

---
+++ src/components/Palette/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import SchemeColor from '../SchemeColor';
+
+const Palette = ( paletteData ) => {
+    const {name, image, attribution, colors, direction, description} = paletteData;
+    console.log(image);
+    return <div className="palette">
+          <div className={`palette-scheme palette-scheme--${direction}`}>
+            <img className="scheme-image" src={image} alt={name} />
+            <div className="scheme-colors">
+             {colors.map((color) => (
+                <SchemeColor key={color} color={color} />
+                ))}
+            </div>
+          </div>
+          <div className="palette-info">
+            <h2>{name}</h2>
+            <p>{description}</p>
+
+            <p>Photo by <a href={attribution.url} target="_blank">{attribution.name}</a>.</p>
+          </div>
+    </div>
+}
+
+export default Palette;
